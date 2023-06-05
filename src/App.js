dc5c3887ef5415e5c5a1310061273c2c27aeec75
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-06-05 18:32:42 +0500

    Add linters setup for react project

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Starting the <strong>Math-Magicians</strong> Project.
         </p>
         <a
           className="App-link"
