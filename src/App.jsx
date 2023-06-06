c52f4f8c0b37822207befba81a81d77a99b6d52b
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-06-06 16:30:29 +0500

    Complete the first part of project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Buttons from './components/Calculator';
+
+function App() {
+  return (
+    <div className="container">
+      <Buttons />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Calculator.jsx
@@ -0,0 +1,63 @@
+const Buttons = () => (
+  <div className="wrapper">
+    <div className="screen">0</div>
+    <div className="btn light-gray">
+      AC
+    </div>
+    <div className="btn light-gray">
+      +/-
+    </div>
+    <div className="btn light-gray">
+      %
+    </div>
+    <div className="btn orange">
+      /
+    </div>
+    <div className="btn">
+      7
+    </div>
+    <div className="btn">
+      8
+    </div>
+    <div className="btn">
+      9
+    </div>
+    <div className="btn orange">
+      X
+    </div>
+    <div className="btn">
+      4
+    </div>
+    <div className="btn">
+      5
+    </div>
+    <div className="btn">
+      6
+    </div>
+    <div className="btn orange">
+      +
+    </div>
+    <div className="btn">
+      1
+    </div>
+    <div className="btn">
+      2
+    </div>
+    <div className="btn">
+      3
+    </div>
+    <div className="btn orange">
+      -
+    </div>
+    <div className="btn zero">
+      0
+    </div>
+    <div className="btn">
+      .
+    </div>
+    <div className="btn orange">
+      =
+    </div>
+  </div>
+);
+export default Buttons;
