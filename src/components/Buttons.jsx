9d2f8afa63ae6e33056961ed4af45bd2dfbd42d4
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-06-07 00:17:44 +0500

    Complete the event part of my calculator porject

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,66 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Buttons = () => (
-  <div className="wrapper">
-    <div className="screen">0</div>
-    <div className="btn light-gray">
-      AC
-    </div>
-    <div className="btn light-gray">
-      +/-
-    </div>
-    <div className="btn light-gray">
-      %
-    </div>
-    <div className="btn orange">
-      /
-    </div>
-    <div className="btn">
-      7
-    </div>
-    <div className="btn">
-      8
-    </div>
-    <div className="btn">
-      9
-    </div>
-    <div className="btn orange">
-      X
-    </div>
-    <div className="btn">
-      4
-    </div>
-    <div className="btn">
-      5
-    </div>
-    <div className="btn">
-      6
-    </div>
-    <div className="btn orange">
-      +
-    </div>
-    <div className="btn">
-      1
-    </div>
-    <div className="btn">
-      2
-    </div>
-    <div className="btn">
-      3
-    </div>
-    <div className="btn orange">
-      -
-    </div>
-    <div className="btn zero">
-      0
-    </div>
-    <div className="btn">
-      .
-    </div>
-    <div className="btn orange">
-      =
-    </div>
-  </div>
+const Buttons = ({ className, text, onClick }) => (
+  <button type="button" className={className} onClick={onClick}>
+    {text}
+  </button>
 );
+
+Buttons.propTypes = {
+  className: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
 
 export default Buttons;
