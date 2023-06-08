7d26d8ee55d986028bdc2fad86520d48c2804a14
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-06-08 23:44:38 +0500

    Fetch API, Cleaned the linter errors and add style for webpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
+import Quotes from './components/quotes';
 
 function App() {
   return (
     <div className="container">
+      <Quotes />
       <Calculator />
     </div>
   );
